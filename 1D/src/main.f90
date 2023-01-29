0e81f71aca67e0edec0441ddc4814375e16885ce
xihuaxu9s <xihuaxu9s@sina.com.cn>
2023-01-29 17:40:20 +0800

    add main.f90

---
+++ 1D/src/main.f90
@@ -0,0 +1,17 @@
+!> Last Modified : 四  1/ 1 07:59:59 1970
+!/*****************************
+!> @file main.f90
+!> @brief 
+!> @author    Xihua Xu
+!> @copyright Beijing IAPCM
+!> @mail      xu_xihua@iapcm.ac.cn 
+!> @date 
+!> 1) Created Time: 日  1/29 17:39:07 2023
+!> 2) Modified Time: 
+!> @details   details
+!> @version   0.001
+!> **************************/
+
+program main
+
+end program main
